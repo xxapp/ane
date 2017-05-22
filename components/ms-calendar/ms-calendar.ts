6b56d908fc002f83da4b4868f396a5cfb33e7f30
xxapp <552062803@qq.com>
2017-05-23 00:29:06 +0800

    calendar 组件增加日期控制

--- components/ms-calendar/ms-calendar.ts
+++ components/ms-calendar/ms-calendar.ts
@@ -5,7 +5,13 @@ avalon.component('ms-calendar', {
     template: __inline('./ms-calendar.html'),
     defaults: {
         $value: moment(),
+        currentMonth: '',
+        currentYear: 0,
         table: [],
+        mutate(action, ...args) {
+            this.$value[action].apply(this.$value, args);
+            this.calcTable();
+        },
         calcTable() {
             let i, j;
             const firstDayOfMonth = this.$value.clone().startOf('month');
@@ -15,9 +21,17 @@ avalon.component('ms-calendar', {
             const lastDay = lastDayOfMonth.day();
             const prevLastDate = lastDayOfPrevMonth.date();
             const lastDate = lastDayOfMonth.date();
+            const table = [];
             let passed = 0;
             for (i = 0; i < 6; i++) {
                 const tableRow = [];
+                if (i === 0) {
+                    tableRow.push(prevLastDate - firstDay + 1);
+                } else if (passed + 1 > lastDate) {
+                    tableRow.push(passed - lastDate);
+                } else {
+                    tableRow.push(passed);
+                }
                 for (j = 0; j < 7; j++) {
                     if (i === 0 && j + 1 < firstDay) {
                         tableRow.push(prevLastDate - firstDay + j + 2);
@@ -27,12 +41,14 @@ avalon.component('ms-calendar', {
                         tableRow.push(++passed);
                     }
                 }
-                this.table.push(tableRow);
+                table.push(tableRow);
             }
-            console.log(this.table.toJSON());
+            this.table = table;
+            this.currentMonth = this.$value.format('MMM');
+            this.currentYear = this.$value.year();
         },
         onInit(event) {
-            this.$value = moment().add(2, 'months');
+            this.$value = moment().subtract(18, 'months');
             this.calcTable();
         }
     }
