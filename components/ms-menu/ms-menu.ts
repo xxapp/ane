0aba1a9122930dfecd970e71339004e60d614e2c
xxapp <552062803@qq.com>
2017-06-01 00:09:46 +0800

    menu 组件，完成基本交互

--- components/ms-menu/ms-menu.ts
+++ components/ms-menu/ms-menu.ts
@@ -3,6 +3,24 @@ import * as avalon from 'avalon2';
 avalon.component('ms-menu', {
     template: __inline('./ms-menu.html'),
     defaults: {
-        menu: []
+        menu: [],
+        selectedKeys: [],
+        openKeys: [],
+        onClick: avalon.noop,
+        handleClick(item, key, keyPath) {
+            if (!item.children || item.children.length === 0) {
+                // 叶子节点
+                //this.selectedKeys.ensure(item.key);
+                this.selectedKeys = [item.key];
+                this.onClick(item, key, keyPath);
+            } else {
+                // 非叶子节点
+                if (this.openKeys.contains(item.key)) {
+                    this.openKeys.remove(item.key);
+                } else {
+                    this.openKeys.push(item.key);
+                }
+            }
+        }
     }
 });
