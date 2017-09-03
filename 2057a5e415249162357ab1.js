84417b6170624a44af6db30b74a49cd6385eb9a5
travis-ci <travis@travis-ci.org>
2017-09-03 14:49:16 +0000

    [ci skip] publish gh-pages

---
+++ 2057a5e415249162357ab1.js
@@ -0,0 +1,22 @@
+webpackJsonpindex([20],{
+
+/***/ 354:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(375);
+
+/***/ }),
+
+/***/ 375:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+exports.__esModule = true;
+exports.name = 'component-demo-CHANGELOG';
+avalon.component(exports.name, { template: "<div><h2>0.1.10 / 2017-09-03</h2>\n<ul>\n<li>dialog \u7EC4\u4EF6\uFF0C\u5B9E\u73B0\u5B8C\u5168\u81EA\u5B9A\u4E49 footer \u529F\u80FD</li>\n</ul>\n<h2>0.1.9 / 2017-09-02</h2>\n<ul>\n<li>tree \u7EC4\u4EF6\uFF0C\u589E\u52A0 expandedKeys \u5C5E\u6027</li>\n</ul>\n<h2>0.1.8 / 2017-08-31</h2>\n<ul>\n<li>select \u7EC4\u4EF6\uFF0C\u4FEE\u590D\u5916\u90E8\u66F4\u65B0 options \u6570\u636E\uFF0C\u4E0D\u4F1A\u91CD\u65B0\u6E32\u67D3\u7684\u95EE\u9898</li>\n</ul>\n<h2>0.1.7 / 2017-08-25</h2>\n<ul>\n<li>tree-select \u7EC4\u4EF6\uFF0C\u589E\u52A0 direction \uFF08\u201C\u4E0B\u62C9\u201D\u65B9\u5411\uFF09\u5C5E\u6027\uFF0C\u540C\u65F6 select/datepicker/timepicker \u4E5F\u589E\u52A0\u4E86\u6B64\u5C5E\u6027</li>\n<li>tree-select \u7EC4\u4EF6\uFF0ConChange \u56DE\u8C03\u4F20\u5165\u66F4\u591A\u4FE1\u606F</li>\n</ul>\n<h2>0.1.6 / 2017-08-23</h2>\n<ul>\n<li>dialog \u7EC4\u4EF6\uFF0C\u4FEE\u590D\u901A\u8FC7\u53C9\u53C9\u5173\u95ED\u540E\u65E0\u6CD5\u518D\u6B21\u6253\u5F00\u7684\u95EE\u9898</li>\n</ul>\n<h2>0.1.5 / 2017-08-20</h2>\n<ul>\n<li>dialog \u7EC4\u4EF6\uFF0C\u589E\u52A0\u81EA\u5B9A\u4E49\u5F39\u51FA\u6846\u6309\u94AE\u6587\u5B57\u7684\u529F\u80FD</li>\n</ul>\n<h2>0.1.4 / 2017-08-16</h2>\n<ul>\n<li>tree \u7EC4\u4EF6\uFF0C\u4FEE\u590D\u5916\u90E8\u66F4\u65B0 tree \u6570\u636E\uFF0C\u4E0D\u4F1A\u91CD\u65B0\u6E32\u67D3\u7684\u95EE\u9898</li>\n<li>tree \u7EC4\u4EF6\uFF0C\u4FEE\u590D onSelect \u56DE\u8C03\u4E8B\u4EF6\u5BF9\u8C61\u5C5E\u6027\u540D\u9519\u8BEF\u7684\u95EE\u9898</li>\n</ul>\n<h2>0.1.3 / 2017-08-14</h2>\n<ul>\n<li>\n<p>table \u7EC4\u4EF6\uFF0C\u589E\u52A0\u81EA\u52A8\u5E8F\u53F7\u5217</p>\n<ms-table-header :widget=\"{title:'\u5E8F\u53F7',type:'index'}\"></ms-table-header>\n<pre><code :skip=\"true\" class=\"language-html\">&lt;ms-table-header :widget=&quot;{title:'\u5E8F\u53F7',type:'index'}&quot;&gt;&lt;/ms-table-header&gt;\n</code></pre>\n</li>\n</ul>\n<h2>0.1.2 / 2017-08-14</h2>\n<ul>\n<li>form \u7EC4\u4EF6\uFF0CvalidateField \u65B9\u6CD5\u53EA\u9700\u4F20\u5B57\u6BB5\u540D\u79F0</li>\n</ul>\n<h2>0.1.1 / 2017-07-28</h2>\n<ul>\n<li>\u6DFB\u52A0\u7EC4\u4EF6\u6587\u6863</li>\n<li>\u89E3\u51B3\u83DC\u5355\u6CA1\u6709 active \u72B6\u6001\u7684\u95EE\u9898</li>\n<li>\u89E3\u51B3\u8868\u5355\u7EC4\u4EF6\u5171\u4EAB\u4E00\u4E2A record \u5BF9\u8C61\u5BFC\u81F4\u7684\u6570\u636E\u9519\u4E71\u95EE\u9898</li>\n<li>loading \u7EC4\u4EF6\uFF0C\u8C03\u6574\u8499\u7248\u9AD8\u5EA6\u83B7\u53D6\u65B9\u5F0F\uFF0C\u5C06\u6574\u4F53loading\u65B9\u6CD5\u901A\u8FC7ane\u5165\u53E3\u6A21\u5757\u66B4\u9732\u51FA\u53BB</li>\n<li>\u7528 less \u66FF\u4EE3 sass \u91CD\u5199\u7EC4\u4EF6\u6837\u5F0F\uFF0C\u5E76\u548C bootstrap less \u6E90\u6587\u4EF6\u7ED3\u5408</li>\n<li>\u589E\u52A0\u7EC4\u4EF6\uFF1Atree/tree-select</li>\n</ul>\n</div>" });
+
+
+/***/ })
+
+});
